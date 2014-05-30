174f94f79d054d6b52cdb0d8a4870c7512816adc
Yuichi Hagio <yhagio87@gmail.com>
2014-05-30 12:06:32 +0900

    Moved back removePost action in PostsController, deleted component post-box.js but not work, created another post-box.js to use sendAction() but removePost is still not working

--- app/components/post-box.js
+++ app/components/post-box.js
@@ -1,14 +1,5 @@
 export default Ember.Component.extend({
-  actions: {
-    removePost: function(item) {
-      if(this.get('session.user') && item.get('user') === this.get('session.user')){
-        // console.log(item);
-        // this.sendAction('action', item);
-        item.deleteRecord();
-        item.save();
-      } else {
-        console.log("You are not this post's author");
-      }
-    }
+  click: function() {
+    this.sendAction('removePost', this.get('item'));
   }
 });
--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -17,6 +17,16 @@ var PostsController = Ember.ArrayController.extend({
           self.set('postBody', '');
         });
       } 
+    },
+    removePost: function(item) {
+      if(this.get('session.user') && item.get('user') === this.get('session.user')){
+        // console.log(item);
+        // this.sendAction('action', item);
+        item.deleteRecord();
+        item.save();
+      } else {
+        console.log("You are not this post's author");
+      }
     }
   },
   charCount: function(){
