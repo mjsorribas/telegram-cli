32b32f8637cf9f4235a312c6993bd9972decd8b4
Yuichi Hagio <yhagio87@gmail.com>
2014-07-01 12:32:38 -0400

    WIP persistent session in applicationRoute

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,15 +1,29 @@
 var ApplicationRoute = Ember.Route.extend({
   beforeModel: function() {
+
     var route = this;
-    return $.get("/api/users/", 
+    
+    if(this.get('session.user')){
+      console.log('Yes');
+      console.log(this.get('session.user'));
+    } else {
+      console.log('No');
+      $.get("/api/users",
       function(response) {
-        // console.log(response);
+        console.log(response);
         var user = route.store.push('user', response.user);
         route.set('session.user', user);
-      },
-      function(error){
-        console.log(error);
       });
+    }
+
+    
+    // var route = this;
+    // return $.get("/api/users",
+    //   function(response) {
+    //     console.log(response);
+    //     var user = route.store.push('user', response.user);
+    //     route.set('session.user', user);
+    //   });
   }
 });
 export default ApplicationRoute;
