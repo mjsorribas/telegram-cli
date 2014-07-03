fbb3fe5c82c05e138297b9e17b765d658c33e761
Yuichi Hagio <yhagio87@gmail.com>
2014-07-02 20:22:11 -0400

    WIP: persisting session

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,14 +1,33 @@
 var ApplicationRoute = Ember.Route.extend({
   beforeModel: function() {
     var route = this;
-    return $.get("/api/checkLoggedIn/",
-      function(response) {
-        console.log(response.user);
-        if(response.user){
-          var user = route.store.push('user', response.user);
-          route.set('session.user', user);
-        }
-      });
+
+    console.log('===== NEW AJAX CALL ======');
+    return $.ajax({
+          url: '/api/checkLoggedIn/',
+          type: 'GET',
+          dataType: 'json',
+          data: {},
+          success: function(response) {
+            console.log('response.user: ' + response.user);
+            if(response.user){
+              var user = route.store.push('user', response.user);
+              route.set('session.user', user);
+            } 
+          },
+          error: function() {
+            console.log('Error');
+          }
+        });
+
+    // $.get("/api/checkLoggedIn/",
+    //   function(response) {
+    //     console.log(response.user);
+    //     if(response.user){
+    //       var user = route.store.push('user', response.user);
+    //       route.set('session.user', user);
+    //     }
+    //   });
   }
 });
 export default ApplicationRoute;
