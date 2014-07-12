3bd9037069ec24bce620b08710ca8f92133d8e53
Yuichi Hagio <yhagio87@gmail.com>
2014-07-12 19:45:56 -0400

    Uncommented the ordering posts

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -1,11 +1,11 @@
 var UserIndexController = Ember.ArrayController.extend({
-  // posts: (function() {
-  //   return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
-  //     sortProperties: ['date'],
-  //     sortAscending: false,
-  //     content: this.get('model.posts')
-  //   });
-  // }).property('model.posts')
+  posts: (function() {
+    return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+      sortProperties: ['date'],
+      sortAscending: false,
+      content: this.get('model.posts')
+    });
+  }).property('model.posts')
 });
 
 export default UserIndexController;
