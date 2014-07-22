75ac8047cab95e26017909851974cdc785e5705a
Yuichi Hagio <yhagio87@gmail.com>
2014-07-22 19:12:46 -0400

    Removed UsersRoute, templates/user/ refactored

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,25 +1,25 @@
 var UserRoute = Ember.Route.extend({
   model: function(user){
     return this.store.find('user', user.username);
+  },
+  afterModel: function(user){
+  	var loggedInUser = this.get('session.user');
+  	console.log('======= Before Model Call ======');
+  	console.log('loggedInUser: '+loggedInUser);
+  	var username = user.id;
+  	console.log('username: '+username);
+    return $.ajax({
+      url: '/api/users/'+username,
+      type: 'GET',
+      dataType: 'json',
+      data: {},//follower: loggedInUser.id
+      success: function(response) {
+        console.log('response: ' + response);
+      },
+      error: function() {
+        console.log('Error');
+      }
+    });
   }
-  // model: function(user){
-  // 	var loggedInUser = this.get('session.user');
-  // 	console.log('======= Before Model Call ======');
-  // 	console.log('loggedInUser: '+loggedInUser);
-  // 	var username = user.id;
-  // 	console.log('username: '+username);
-  //   return $.ajax({
-  //     url: '/api/users/'+username,
-  //     type: 'GET',
-  //     dataType: 'json',
-  //     data: {},//follower: loggedInUser.id
-  //     success: function(response) {
-  //       console.log('response: ' + response);
-  //     },
-  //     error: function() {
-  //       console.log('Error');
-  //     }
-  //   });
-  // }
 });
 export default UserRoute;
