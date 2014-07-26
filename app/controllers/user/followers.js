dcd12e584b9532bdc4a6674d66dda871888d1ac5
Yuichi Hagio <yhagio87@gmail.com>
2014-07-26 10:57:24 -0400

    WIP: Real-time update of followers

--- app/controllers/user/followers.js
+++ app/controllers/user/followers.js
@@ -1,9 +1,29 @@
 var UserFollowersController = Ember.ArrayController.extend({
+	needs: 'user', // Load UserRoute's returned users (followers)
+	followee: Ember.computed.alias('controllers.user.model'),
 	followers: function(){
-		// console.log(" === ==== UserFollowersController ==== ===");
-		// console.log(this);
-		return this;
-  }.property('@each.followedByCurrentUser')
+		var followee = this.get('followee');
+		var authenticatedUser = this.get('session.user');
+
+		if(followee.get('followedByCurrentUser')){ // following now
+			// Include authenticated user
+			var users = [];
+			users.push(this);
+			if(this.indexOf(authenticatedUser) < 0){
+				users[0].push(authenticatedUser);
+			}
+			return users[0];
+		} else { // not following yet
+			// Exclude authenticated user 
+			var users = this;
+			var index = users.indexOf(authenticatedUser);
+			if(index >= 0){
+				// users.splice(index, 1);		
+				delete users[authenticatedUser];
+			}
+			return users;
+		}
+  }.property('followee.followedByCurrentUser', '[]') // @each or []
 });
 
 export default UserFollowersController;
