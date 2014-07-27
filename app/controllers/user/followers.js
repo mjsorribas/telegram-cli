76511795ea44b92ff477bc0bb452bc4c3f34796f
Yuichi Hagio <yhagio87@gmail.com>
2014-07-27 12:18:29 -0400

    WIP: continued ...

--- app/controllers/user/followers.js
+++ app/controllers/user/followers.js
@@ -1,29 +1,49 @@
+// var UserFollowersController = Ember.ArrayController.extend({
+// 	needs: 'user', // Load UserRoute's returned users (followers)
+// 	followee: Ember.computed.alias('controllers.user.model'),
+// 	followers: function(){
+// 		var followee = this.get('followee');
+// 		var authenticatedUser = this.get('session.user');
+
+// 		if(followee.get('followedByCurrentUser')){ // following now
+// 			// Include authenticated user
+// 			var users = [];
+// 			users.push(this);
+// 			if(this.indexOf(authenticatedUser) < 0){
+// 				users.push(authenticatedUser);
+// 			}
+// 			return users;
+// 		} else { // not following yet
+// 			// Exclude authenticated user 
+// 			var users = this;
+// 			var index = users.indexOf(authenticatedUser);
+// 			if(index >= 0){
+// 				// users.splice(index, 1);		
+// 				delete users[authenticatedUser];
+// 			}
+// 			return users;
+// 		}
+//   }.property('followee.followedByCurrentUser', '@each') // @each or []
+// });
+
+// export default UserFollowersController;
+
+
 var UserFollowersController = Ember.ArrayController.extend({
-	needs: 'user', // Load UserRoute's returned users (followers)
+	needs: 'user',
 	followee: Ember.computed.alias('controllers.user.model'),
 	followers: function(){
 		var followee = this.get('followee');
 		var authenticatedUser = this.get('session.user');
 
-		if(followee.get('followedByCurrentUser')){ // following now
-			// Include authenticated user
-			var users = [];
-			users.push(this);
-			if(this.indexOf(authenticatedUser) < 0){
-				users[0].push(authenticatedUser);
-			}
-			return users[0];
-		} else { // not following yet
-			// Exclude authenticated user 
-			var users = this;
-			var index = users.indexOf(authenticatedUser);
-			if(index >= 0){
-				// users.splice(index, 1);		
-				delete users[authenticatedUser];
-			}
+		var users = [];
+		users.push(this);
+		if(followee.get('followedByCurrentUser')){
+			// this === this.get('followers')
+			return users;
+		} else {
 			return users;
 		}
-  }.property('followee.followedByCurrentUser', '[]') // @each or []
+  }.property('followee.followedByCurrentUser', '@each')
 });
-
 export default UserFollowersController;
