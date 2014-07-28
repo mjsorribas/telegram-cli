78012004a506905dc68cef8fa3588911b94e23d0
Yuichi Hagio <yhagio87@gmail.com>
2014-07-27 21:56:07 -0400

    Fixed: real-time update followers of non-loggedin user

--- app/controllers/user/followers.js
+++ app/controllers/user/followers.js
@@ -1,47 +1,26 @@
-// var UserFollowersController = Ember.ArrayController.extend({
-// 	needs: 'user', // Load UserRoute's returned users (followers)
-// 	followee: Ember.computed.alias('controllers.user.model'),
-// 	followers: function(){
-// 		var followee = this.get('followee');
-// 		var authenticatedUser = this.get('session.user');
-
-// 		if(followee.get('followedByCurrentUser')){ // following now
-// 			// Include authenticated user
-// 			var users = [];
-// 			users.push(this);
-// 			if(this.indexOf(authenticatedUser) < 0){
-// 				users.push(authenticatedUser);
-// 			}
-// 			return users;
-// 		} else { // not following yet
-// 			// Exclude authenticated user 
-// 			var users = this;
-// 			var index = users.indexOf(authenticatedUser);
-// 			if(index >= 0){
-// 				// users.splice(index, 1);		
-// 				delete users[authenticatedUser];
-// 			}
-// 			return users;
-// 		}
-//   }.property('followee.followedByCurrentUser', '@each') // @each or []
-// });
-
-// export default UserFollowersController;
-
-
 var UserFollowersController = Ember.ArrayController.extend({
 	needs: 'user',
 	followee: Ember.computed.alias('controllers.user.model'),
 	followers: function(){
-		var followee = this.get('followee');
-		var authenticatedUser = this.get('session.user');
-
-		var users = [];
-		users.push(this);
+		var followee 					= this.get('followee'),
+				authenticatedUser = this.get('session.user'),
+				users = [];
+		// Update the followers list of the user whom authenticatedUser is visisting now
 		if(followee.get('followedByCurrentUser')){
-			// this === this.get('followers')
+			this.forEach(function(object) {
+				users.push(object);
+			});
+			if(users.indexOf(authenticatedUser) < 0){
+				users.push(authenticatedUser);
+			}
 			return users;
 		} else {
+			this.forEach(function(object) {
+				users.push(object);
+			});
+			if(users.indexOf(authenticatedUser) >= 0){
+				users.splice(users.indexOf(authenticatedUser), 1);
+			}
 			return users;
 		}
   }.property('followee.followedByCurrentUser', '@each')
