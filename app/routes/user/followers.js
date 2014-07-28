53bf857e77a3aebe560e6e5b2ddbabd11dbc4e36
Yuichi Hagio <yhagio87@gmail.com>
2014-07-27 23:47:40 -0400

    Real-time update followees of logged-in user

--- app/routes/user/followers.js
+++ app/routes/user/followers.js
@@ -1,11 +1,7 @@
 var UserFollowersRoute = Ember.Route.extend({
 	// Filter users by checking if containing currentURLuser as a followee
 	model: function(){
-		// return this.modelFor('user');
-
   	var currentURLuser = this.modelFor('user');
-  	// return this.store.find('user', {followings: currentURLuser.id});
-  	// console.log("= = = = = = = = =");
   	return this.store.find( "user",
 		  {
 		    followee: currentURLuser.id // currentUserAsFollowee
