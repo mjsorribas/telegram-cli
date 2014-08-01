48e4f298a3bd8ac927b46220e68eb40335b44b0e
Yuichi Hagio <yhagio87@gmail.com>
2014-08-01 04:59:33 -0400

    redirecting non-loggedin users

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,36 +1,11 @@
 var PostsRoute = Ember.Route.extend({
+  beforeModel: function(){
+    if(!this.get('session.user')) {
+      this.transitionTo('auth.login');
+    }
+  },
   model: function(){
     return this.store.find('post');
   }
-  // model: function() {
-  //   var authenticatedUser = this.get('session.user');
-  //   var that = this;
-  //   return this.store.find('user', 
-  //     {
-  //       follower: authenticatedUser.id,
-  //       limit   : 20
-  //       // skip    : 20
-  //     }
-  //   ).then(function(users){
-  //     var followingUsers = [];
-  //     users.forEach(function(eachUser){
-  //       if(followingUsers.indexOf(eachUser.get('id')) < 0){
-  //         followingUsers.push(eachUser.get('id'));
-  //       }
-  //     });
-
-  //     return that.store.filter(
-  //       "post",
-  //       {
-  //         limit: 20
-  //         // skip : 20
-  //       },
-  //       function(post) {
-  //         var author = post.get('user.id');
-  //         return followingUsers.indexOf(author) >= 0;
-  //       }
-  //     );
-  //   });
-  // }
 });
 export default PostsRoute;
